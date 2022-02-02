066ad31af71c7e9165f3c5455fb0047b65fe7461
OSCAR-OD <64194765+OSCAR-OD@users.noreply.github.com>
2022-02-02 09:15:18 +0600

    first practice
    first practice

---
+++ php1/index.php
@@ -0,0 +1,32 @@
+
+<html>
+
+<head>
+</head>
+<Body>
+<h1></h1>
+<form>
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fame"><br>
+  
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br>
+  
+   <label for="age">Age:</label>
+  <input type="text" id="age" name="age"><br>
+  
+  <input type="radio" id="jprogrammer" name="programmer" value="jprogrammer">
+  <label for="jprogrammer">Junior programmer</label>
+  <input type="radio" id="sprogrammer" name="programmer" value="sprogrammer">
+  <label for="sprogrammer">Sensior programmer</label>
+  <input type="radio" id="projectlead" name="programmer" value="projectlead">
+  <label for="projectlead">Project Lead</label>
+ <label for="file">file choose</label>
+  <input type="file" id="file" name="file" value="file">
+ 
+<input type="submit" value="Submit">
+<input type="Reset" value="Reset">
+</form>
+</Body>
+
+</html>
